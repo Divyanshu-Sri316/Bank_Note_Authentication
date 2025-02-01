3cc7d544f8453a3ce77aa22cde89eb828884c816
Divyanshu-Sri316 <43092613+Divyanshu-Sri316@users.noreply.github.com>
2025-02-01 11:42:51 +0530

    Web App completed

--- app.py
+++ app.py
@@ -1,38 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 19 18:47:30 2023
-
-@author: Asus
-"""
-
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 import pickle
 
 app = Flask(__name__)
+
 pickle_in = open('classifier.pkl', "rb")
 classifier = pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
-    return "Welcome All"
+    return render_template('index.html')  # This loads the HTML file from templates
 
-@app.route('/predict',methods=["GET"])
+@app.route('/predict', methods=["GET"])
 def predict_note_authentication():
-    variance = request.args.get('variance')
-    skewness = request.args.get('skewness')
-    curtosis = request.args.get('curtosis')
-    entropy  = request.args.get('entropy')
-    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
-    return "The predicted value is" + str(prediction)
+    variance = request.args.get('variance', type=float)
+    skewness = request.args.get('skewness', type=float)
+    curtosis = request.args.get('curtosis', type=float)
+    entropy = request.args.get('entropy', type=float)
+    
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+    return f"The predicted value is {prediction[0]}"
 
-@app.route('/predict_file',methods=["POST"])
+@app.route('/predict_file', methods=["POST"])
 def predict_note_file():
     df_test = pd.read_csv(request.files.get("file"))
     prediction = classifier.predict(df_test)
-    return "The predicted value is" + str(list(prediction))
-
+    return f"The predicted values are {list(prediction)}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
